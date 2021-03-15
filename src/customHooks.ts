cdc5cb72f819e1463dde10bf5e4abe70c5e34b63
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2021-03-15 17:22:31 +0530

    useInterval

--- src/customHooks.ts
+++ src/customHooks.ts
@@ -3,6 +3,7 @@ import {
   Router,
   useEffect,
   RouterSubscription,
+  useRef,
 } from "@hydrophobefireman/ui-lib";
 
 function useMount(fn: () => unknown | (() => void)) {
@@ -33,4 +34,24 @@ export function useViewportSize(): [number, number] {
   });
 
   return dimensions;
+}
+
+export function useInterval(callback: () => void, delay: number) {
+  const savedCallback = useRef<() => void>();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
 }
